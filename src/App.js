d3033e147ba261c5e91c261cb03be5bf64b34b1d
Your Name <r.naveenprakash2942004@gmail.com>
2023-03-31 15:33:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FruitSurvey from './component/three';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FruitSurvey />
+      </header>
+    </div>
+  );
+}
+
+export default App;
